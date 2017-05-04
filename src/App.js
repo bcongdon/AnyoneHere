e8173fcf6636886bd2d20a02b5395e4c07b2410f
Benjamin Congdon <bencongdon96@gmail.com>
2017-05-04 11:18:13 -0500

    Script fixes, css fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import UserTable from './UserTable'
+import UserTable from './UserTable';
+import css from './App.css';
 
 class App extends Component {
   render() {
